e7be9f8b7eecb4045a4a10295c784f3ca29834a2
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-26 21:06:48 +0200

    added @Before advice and displayed the method we are calling. also fixed issue in pointcuts with an extra dot

--- src/com/javi/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/javi/springdemo/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package com.javi.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +19,13 @@ public class CRMLoggingAspect {
 	
 	
 	// setup pointcut declarations
-	@Pointcut("execution(* com.javi.springdemo.controller.*.*.(..)")// * : Match on any CLASS in the package. * : match on any METHOD in the class. .. : Match on any number of arguments
+	@Pointcut("execution(* com.javi.springdemo.controller.*.*(..)")// * : Match on any CLASS in the package. * : match on any METHOD in the class. .. : Match on any number of arguments
 	private void forControllerPackage()  {}
 	
-	@Pointcut("execution(* com.javi.springdemo.service.*.*.(..)")// * : Match on any CLASS in the package. * : match on any METHOD in the class. .. : Match on any number of arguments
+	@Pointcut("execution(* com.javi.springdemo.service.*.*(..)")// * : Match on any CLASS in the package. * : match on any METHOD in the class. .. : Match on any number of arguments
 	private void forServicePackage()  {}
 	
-	@Pointcut("execution(* com.javi.springdemo.dao.*.*.(..)")// * : Match on any CLASS in the package. * : match on any METHOD in the class. .. : Match on any number of arguments
+	@Pointcut("execution(* com.javi.springdemo.dao.*.*(..)")// * : Match on any CLASS in the package. * : match on any METHOD in the class. .. : Match on any number of arguments
 	private void forDaoPackage()  {}
 	
 	
@@ -31,6 +33,17 @@ public class CRMLoggingAspect {
 	private void forAppFlow()  {}
 	
 	// add @Before advice
+	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// display method we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("======> in @Before: calling method: " + theMethod);
+		
+		// display the arguments to the method
+		
+	}
 	
 	// add @AfterReturning advice
 
