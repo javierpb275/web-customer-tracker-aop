0196d072338dba94289f2581a7c7102d32067e0e
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-26 20:22:19 +0200

    added logging support

--- src/com/javi/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/javi/springdemo/aspect/CRMLoggingAspect.java
@@ -1,10 +1,23 @@
 package com.javi.springdemo.aspect;
 
+import java.util.logging.Logger;
+
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+
 
 @Aspect
 @Component
 public class CRMLoggingAspect {
+	
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	// setup pointcut declarations
+	
+	// add @Before advice
+	
+	// add @AfterReturning advice
 
 }
